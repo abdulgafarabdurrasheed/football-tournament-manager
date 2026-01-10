667ed544da968569096f66bd6ce1d62df796064b
abdulgafarabdurrasheed <narutoshippudeen124@gmail.com>
2026-01-10 15:12:24 +0100

    created Button component, added spinners for loading state

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import Button from './components/ui/button'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Button />
   </StrictMode>,
 )
 
