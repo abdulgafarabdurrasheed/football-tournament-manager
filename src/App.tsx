642615222a39e7e3f58240fba1123e8168266c7a
abdulgafarabdurrasheed <narutoshippudeen124@gmail.com>
2026-01-11 03:41:13 +0100

    learning, and practiicing useState by creating a ColorChanger

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import Card from './components/ui/Card';
+import Card from './components/ui/card';
+import ColorChanger from './components/practice/ChangeColor'
 
 function App(){
     return (
         <div className="p-8 space-y-4">
             <Card title="Manager Cards">Manager Info</Card>
             <Card hoverable onClick={() => alert('Clicked!')}>Clickable card</Card>
+            <ColorChanger />
         </div>
     )
 }
